723c6e7d9ba2004c2e3cf98d24bc96bdaf4fa4f4
Komeshan <komeshan002@gmail.com>
2026-05-20 22:04:34 +0530

    setting up resumeBuilder page

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { dummyResumeData } from '../assets/assets'
 
 const ResumeBuilder = () => {
+
+  const { resumeId } = useParams()
+
+  const [ resumeData, setResumeData] = React.useState({
+    _id: '',
+    title: '',
+    personal_info: {},
+    professional_summary: '',
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: 'classic',
+    accent_color: '#9333ea',
+    public: false,
+  })
+
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find(resume._id === resumeId)
+    if(resume) {
+      setResumeData(resume)
+      document.title = resume.title
+    }
+  }
+
+  useEffect(() => {
+    loadExistingResume()
+  },[])
+
+
   return (
     <div>
-      <h1>Resume Builder</h1>
+      
     </div>
   )
 }
