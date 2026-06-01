6ce29589f70b94ada9b6cb049716def3ec9c87a6
Komeshan <komeshan002@gmail.com>
2026-06-01 19:29:12 +0530

    created Loader.jsx and Preview.jsx

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets'
+import ResumePreview from '../components/ResumePreview'
+import Loader from '../components/Loader'
+import { ArrowLeftIcon } from 'lucide-react'
 
 const Preview = () => {
-  return (
+
+  const {resumeId} = useParams()
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [resumeData, setResumeData] = useState(null)
+
+  const loadResumeData = async () => {
+    setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null))
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    loadResumeData()
+  },[])
+
+  return resumeData ? (
+    <div className='bg-slate-100'>
+      <div className='max-w-3xl mx-auto py-10'>
+        <ResumePreview classes='py-4 bg-white' data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color}/>
+      </div>
+    </div>
+  ) : (
     <div>
-      <h1>Preview page</h1>
+      {isLoading ? <Loader/> : (
+        <div className='flex flex-col items-center justify-center h-screen'>
+          <p className='text-center text-6xl text-slate-400 font-medium'>Resume not found</p>
+          <a href="/app" className='mt-6 bg-blue-500 hover:bg-blue-600 flex items-center text-white border px-6 h-9 m-1 ring-offset-1 ring-1 ring-blue-400 rounded-full transition-colors'>
+            <ArrowLeftIcon className='mr-2 size-4'/>
+            go to dashboard page
+          </a>
+        </div>
+      )}
     </div>
   )
 }
--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FoldersIcon, GraduationCap, Sparkles, User } from 'lucide-react'
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, DownloadIcon, EyeIcon, EyeOff, FileText, FoldersIcon, GraduationCap, Share2Icon, Sparkles, User } from 'lucide-react'
 import { dummyResumeData } from '../assets/assets'
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
@@ -56,6 +56,41 @@ const ResumeBuilder = () => {
     loadExistingResume()
   },[])
 
+  const changeResumeVisibility = async () => {
+    setResumeData ({...resumeData, public: !resumeData.public})
+  }
+
+  const handleShare = async () => {
+  console.log("clicked share");
+
+  const resumeUrl = `${window.location.origin}/view/${resumeId}`;
+
+  try {
+    if (!navigator.share) {
+      await navigator.clipboard.writeText(resumeUrl);
+      alert("Copied to clipboard");
+      return;
+    }
+
+    await navigator.share({
+      title: "My Resume",
+      text: "Check this out",
+      url: resumeUrl,
+    });
+
+  } catch (err) {
+    console.log("Share error:", err);
+
+    if (err.name !== "AbortError") {
+      await navigator.clipboard.writeText(resumeUrl);
+      alert("Share failed, link copied");
+    }
+  }
+};
+ 
+  const downloadResume = () => {
+    window.print()
+  }
 
   return (
     <div>
@@ -126,16 +161,39 @@ const ResumeBuilder = () => {
                 )}
                 
               </div>
+
+              <button className='bg-gradient-to-br from-green-400 to-green-200 ring-green-300 text-green-950 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+                Save Changes
+              </button>
+
             </div>
           </div>
 
           {/* Right panel - Resume preview */}
           <div className='lg:col-span-7 max-lg:mt-6'>
-            <div>
-              {/* buttons */}
+
+            <div className='relative w-full'>
+              <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
+                {resumeData.public && (
+                  <button onClick={handleShare} className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors'>
+                    <Share2Icon className='size-4'/> Share
+                  </button>
+                )}
+
+                <button onClick={changeResumeVisibility} className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 rounded-lg ring-purple-300 hover:ring transition-colors'>
+                  {resumeData.public ? <EyeIcon className='size-4'/> : <EyeOff className='size-4'/>}
+                  {resumeData.public ? 'public' : 'private'}
+                </button>
+
+                <button onClick={downloadResume} className='flex items-center gap-2 px-6 py-2 text-xs bg-gradient-to-br from-green-100 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
+                  <DownloadIcon className='size-4'/> Download
+                </button>
+
+              </div>
             </div>
 
             <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color} />
+          
           </div>
 
         </div>
